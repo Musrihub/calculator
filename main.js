842102acb02eff8226e45d0f3c26d028814b160a
Lionel NTIRUSHWA <linog02@gmail.com>
2023-07-23 18:00:31 +0200

    added calculator functionality

---
+++ main.js
@@ -0,0 +1,25 @@
+const srceenDisplay = document.querySelector('.screen')
+const buttons = document.querySelectorAll('button')
+
+let calculation = []
+let accumulativeCalculation
+
+function calculate(button) {
+    const value = button.textContent
+    if (value === "CLEAR") {
+        calculation = []
+        srceenDisplay.textContent = '.'
+    }
+    else if (value === "=") {
+        srceenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value)
+        accumulativeCalculation = calculation.join('')
+        srceenDisplay.textContent = accumulativeCalculation
+        console.log(calculation)
+    }
+
+
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
