abe6189ff3af4301d58211e4f9df4b2034e0858e
Lionel NTIRUSHWA <linog02@gmail.com>
2023-08-09 17:34:56 +0200

    modified style

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-const srceenDisplay = document.querySelector('.screen')
-const buttons = document.querySelectorAll('button')
+const srceenDisplay = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
 
-let calculation = []
-let accumulativeCalculation
+let calculation = [];
+let accumulativeCalculation;
 
 function calculate(button) {
-    const value = button.textContent
-    if (value === "CLEAR") {
-        calculation = []
-        srceenDisplay.textContent = '.'
-    }
-    else if (value === "=") {
-        srceenDisplay.textContent = eval(accumulativeCalculation)
-    } else {
-        calculation.push(value)
-        accumulativeCalculation = calculation.join('')
-        srceenDisplay.textContent = accumulativeCalculation
-        console.log(calculation)
-    }
-
-
+  const value = button.textContent;
+  if (value === "CLEAR") {
+    calculation = [];
+    srceenDisplay.textContent = ".";
+  } else if (value === "=") {
+    srceenDisplay.textContent = eval(accumulativeCalculation);
+  } else {
+    calculation.push(value);
+    accumulativeCalculation = calculation.join("");
+    srceenDisplay.textContent = accumulativeCalculation;
+    console.log(calculation);
+  }
 }
 
-buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
+buttons.forEach((button) =>
+  button.addEventListener("click", () => calculate(button))
+);
