b952c37729be19452eed35afe221ce3ccc422662
Lionel NTIRUSHWA <linog02@gmail.com>
2023-08-09 20:35:56 +0200

    modified files

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-const srceenDisplay = document.querySelector(".screen");
-const buttons = document.querySelectorAll("button");
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelectorAll(".btn");
+  let clear = document.querySelector("#clear");
+  let equal = document.querySelector("#equal");
 
-let calculation = [];
-let accumulativeCalculation;
-
-function calculate(button) {
-  const value = button.textContent;
-  if (value === "CLEAR") {
-    calculation = [];
-    srceenDisplay.textContent = ".";
-  } else if (value === "=") {
-    srceenDisplay.textContent = eval(accumulativeCalculation);
-  } else {
-    calculation.push(value);
-    accumulativeCalculation = calculation.join("");
-    srceenDisplay.textContent = accumulativeCalculation;
-    console.log(calculation);
-  }
-}
-
-buttons.forEach((button) =>
-  button.addEventListener("click", () => calculate(button))
-);
+  buttons.forEach(function (button) {
+    button.addEventListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+      // console.log(value);
+    });
+  });
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+  clear.addEventListener("click", function () {
+    screen.value = "";
+  });
+})();
